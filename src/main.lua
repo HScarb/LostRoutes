b52f27e89403ab30ac01cdca56281e2be80e4d4e
scarb_work <jjhfen00@163.com>
2017-04-27 11:58:49 +0800

    ReInit.

--- src/main.lua
+++ src/main.lua
@@ -1,39 +1,16 @@
 
--- cc.FileUtils:getInstance():setPopupNotify(false)
-cc.FileUtils:getInstance():addSearchPath("src/")
-cc.FileUtils:getInstance():addSearchPath("res/")
-cc.FileUtils:getInstance():addSearchPath("res/hd")
-cc.FileUtils:getInstance():addSearchPath("res/fonts")
-cc.FileUtils:getInstance():addSearchPath("res/particle")
-cc.FileUtils:getInstance():addSearchPath("res/sound")
+cc.FileUtils:getInstance():setPopupNotify(false)
 
 require "config"
 require "cocos.init"
 
--- cclog
-local cclog = function(...)
-    print(string.format(...))
-end
-
--- for CCLuaEngine traceback
-function __G__TRACKBACK__( msg )
-    cclog("--------------------------------------")
-    cclog("LUA-ERROR: " .. tostring(msg) .. "\n")
-    cclog(debug.traceback())
-    cclog("--------------------------------------")
-    return msg
-end
-
 local function main()
-    -- require("app.MyApp"):create():run()
-    collectgarbage("collect")
-    -- avoid memory leak
-    collectgarbage("setpause", 100)
-    collectgarbage("setstepmul", 5000)
+    local director = cc.Director:getInstance()
+    local glview = director:getOpenGLView()
+    local frameSize = glview:getFrameSize()
+    print(frameSize.height, frameSize.width)
 
-    -- initialize director
-    local director = cc.director:getInstance()
-    
+    require("app.MyApp"):create():run()
 end
 
 local status, msg = xpcall(main, __G__TRACKBACK__)
